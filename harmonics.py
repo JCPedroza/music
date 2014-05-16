4f100ee2d9bfbdbb2a8243630bb0977ed728240a
JCPedroza <nemesis4go10@hotmail.com>
2014-05-16 05:29:25 -0600

    now works

--- harmonics.py
+++ harmonics.py
@@ -1,49 +1,5 @@
 # !!! Need more accurete floating points?
 
-# One octave of equal-temperament frequencies
-C  = 32.703194
-Db = 34.647827
-D  = 36.708094
-Eb = 38.890871
-E  = 41.203443
-F  = 43.653527
-Gb = 46.249301
-G  = 48.999427
-Ab = 51.913085
-A  = 55.000000
-Bb = 58.270468
-B  = 61.735410
-
-# Dictionary of <note name>: <frequency>
-freqDic = {"C": C, "Db": Db, "D": D, "Eb": Eb, "E": E, "F": F,
-        "Gb": Gb, "G": G, "Ab": Ab, "A": A, "Bb": Bb, "B": B}
-
-# Dictionary of <step>: <interval>
-intervals = ["root", "minor second", "major second", "minor third", "major third", "fourth",  
-        "tritone", "fifth", "minor sixth", "major sixth", "minor seventh", "major seventh"]
-
-# Array of note names
-noteNames = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
-
-
-def step_count(note1, note2):
-    """
-    Counts the number of steps between two notes.
-    """
-    global noteNames
-    count = noteNames.index(note2) - noteNames.index(note1)
-    if count > 0:
-        return count
-    else:
-        return count + len(noteNames)
-
-def step_to_interval(step):
-    """
-    Converts a step to an interval.
-    """
-    global intervals
-    return intervals[step]
-
 def generate_harmonics(root, depth):
     """
     Returns an array with <depth> harmonics of the root frequency.
@@ -60,16 +16,56 @@ def generate_intervals(root):
     equal-temperament. 
     """
     ratio   = 2**(1/12.0)   # ratio in equal temperament
-    results = []
+    results = [root]
     value   = root
     for i in range(11):
         value = value * ratio
         results.append(value)
     return results
 
-# def categorize(root, depth):
-#     harmonics = generate_harmonics(root, depth)
-#     results   = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#     for i in harmonics:
+def categorize(root, depth):
+    harmonics  = generate_harmonics(root, depth)
+    intervals  = generate_intervals(root)
+    results    = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    last       = 0
+    current    = 0
 
-print generate_intervals(32.703194)
+    for harmonic in harmonics:
+        
+        # If the harmonic is out of range of the current octave, create a new list of 
+        # intervals for the next octave.
+        if harmonic > intervals[-1]:
+            intervals = [n * 2 for n in intervals]
+
+        # Search for the closest interval
+        for interval in range(len(intervals)):
+            current = harmonic - intervals[interval]                     
+            if current < 0:                
+                current_difference = abs(current)
+                last_difference = abs(last)
+                if current_difference < last_difference:                                        
+                    results[interval] += 1
+                else:                                        
+                    results[interval - 1] += 1                                
+                break
+            else:
+                last = current
+
+    return results;
+
+def print_unordered_results(root, depth):
+    interval_names = ["root          ", "minor second  ", "major second  ", "minor third   ", "major third   ", "fourth        ",  
+        "tritone       ", "fifth         ", "minor sixth   ", "major sixth   ", "minor seventh ", "major seventh "]
+    results = categorize(root, depth)
+    for i in range(len(results)):
+        print interval_names[i], results[i]
+
+print "55 11th harmonic: " , 55 * 11
+print_unordered_results(55, 11)
+
+
+
+
+
+
+
