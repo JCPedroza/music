28a69ae271eb5d1cf668cee4a7e7ce4a51e33d60
JCPedroza <nemesis4go10@hotmail.com>
2014-05-16 00:37:31 -0600

    freqs and name freq array

---
+++ harmonics.py
@@ -0,0 +1,18 @@
+# One octave of equal-temperament frequencies
+C  = 32.703194
+Db = 34.647827
+D  = 36.708094
+Eb = 38.890871
+E  = 41.203443
+F  = 43.653527
+Gb = 46.249301
+G  = 48.999427
+Ab = 51.913085
+A  = 55.000000
+Bb = 58.270468
+B  = 61.735410
+
+# Dictionary <note name>: <frequency>
+freqDic = {"C": C, "Db": Db, "D": D, "Eb": Eb, "E": E, "F": F,
+        "Gb": Gb, "G": G, "Ab": Ab, "A": A, "Bb": Bb, "B": B}
+
